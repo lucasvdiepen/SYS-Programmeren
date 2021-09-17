674ce44d4366064a46a837a3d56b72e015249531
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-09-17 10:22:47 +0200

    Added EnemyClass

---
+++ EnemyClass/EnemyClass/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace EnemyClass
+{
+    class Program
+    {
+        private class Enemy
+        {
+            public int health { get; private set; }
+
+            public Enemy(int _health)
+            {
+                health = _health;
+            }
+
+            public void TakeDamage(int damage)
+            {
+                if(health > 0)
+                {
+                    health -= damage;
+                    if (health <= 0) Console.WriteLine("Enemy died");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Enemy enemy = new Enemy(100);
+
+            Random rnd = new Random();
+
+            while (true)
+            {
+                enemy.TakeDamage(rnd.Next(5, 20));
+
+                Console.WriteLine("Enemy health: " + enemy.health);
+
+                if (enemy.health <= 0) break;
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
