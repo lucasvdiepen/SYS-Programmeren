b92975809700cad597aa51f984b774a035db3b56
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-10-01 10:08:38 +0200

    Added RoomClass

---
+++ RoomClass/RoomClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RoomClass
+{
+    class Program
+    {
+        private class Tile
+        {
+            
+        }
+
+        private class Room
+        {
+            private Tile[][] tiles;
+
+            public Room(int width, int height)
+            {
+                this.tiles = new Tile[height][];
+
+                for (int i = 0; i < height; i++)
+                {
+                    this.tiles[i] = new Tile[width];
+
+                    for(int j = 0; j < width; j++)
+                    {
+                        this.tiles[i][j] = new Tile();
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Room room = new Room(10, 10);
+        }
+    }
+}
